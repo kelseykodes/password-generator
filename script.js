0100cfa8561d373a44f576b08450851876d36fa5
kperkins97 <perkins.kelsey@yahoo.com>
2021-09-12 16:27:56 -0500

    prompts

---
+++ script.js
@@ -0,0 +1,27 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+ function generatePassword() {
+  function promptThis(){
+      //uppercase letters
+      var upperCase = prompt("Enter two uppercase letters from A-Z ");
+      //lowercase letters 
+      var lowerCase = prompt("Enter two lowercase letters from a-z ");
+      //symbols
+      var sympbols = prompt("Enter two of the following symbols: ! @ # $ % ^ & or * ");
+      //numbers
+      var numbers = prompt("Enter two numbers between 0-9 ");
+      //password length
+      //var passLength = prompt("How long would you like your password to be? (Enter a number between 8-128 ");
+  }
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword) 
