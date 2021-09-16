9547813a3eb3fcc29c3bd5fdb4559e2b437c132b
kperkins97 <perkins.kelsey@yahoo.com>
2021-09-15 22:06:27 -0500

    trying to display password text

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var totalOptions = [];
+var randomPassword = ["A", "B", "C", "D", "1", "2", "!", "%"];
+//possible options to take user input to display in text?
+//totalOptions.push(randomPassword);
 
+ generateBtn.addEventListener("click", writePassword) 
+// Write password to the #password input
+function writePassword() {
+generatePassword();
+  var passwordText = document.querySelector("#password");
 
- //function generatePassword() {
-   // var generateBtn = document.querySelector("#generate");
-  //  const passwordValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
-  //  var password = "";
-  // for (var i = 0; i <= generateBtn; i++){
-      //password = password + passwordValues.charCode(Math.floor(Math.random() * Math.floor(passwordValues.length - 1)));
-  // }
-  //document.getElementById("display").value = password;
-      //}
+  passwordText.value = password;
+}
 
   function generatePassword(){
       //uppercase letters
@@ -23,28 +25,26 @@ var generateBtn = document.querySelector("#generate");
       var numbers = prompt("Enter two numbers between 0-9 ");
       //password length
       var passLength = prompt("How long would you like your password to be? (Enter a number between 8-128 ");
+      if (passLength < 8) {
+        window.alert('Password must contain between 8 - 129 characters')
+        return;
+    }
+      if (passLength > 128) {
+        window.alert("Password must contain a maximum of 129 characters");
+        return;
+    } 
   }
-  if (passLength < 8) {
-    window.alert('Password must contain between 8 - 129 characters')
-    return;
-}
-  if (passLength > 128) {
-    window.alert("Password must contain a maximum of 129 characters");
-    return;
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword) 
-
-//  if (passLength < 8) || (passLength > 128){
+// if (passLength < 8) || (passLength > 128){
   //window.alert('Password must contain between 8 - 129 characters')
  // return null;
 
+  //function generatePassword() {
+   // var generateBtn = document.querySelector("#generate");
+  //  const passwordValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+  //  const password = "";
+  //  var firstPassword = password
+  // for (var i = 0; i <= generateBtn; i++){
+     //password = password + passwordValues.charCode(Math.floor(Math.random() * Math.floor(passwordValues.length - 1)));
+  // }
+  //document.getElementById("display").value = password;
+//}
